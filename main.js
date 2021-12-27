0db8e98b183c37e75b1bb789943f5a4ded86d9b8
Darth-Ness <jaycshflw@abine.org>
2021-12-27 06:59:58 -0500

    Add shortcuts.

--- main.js
+++ main.js
@@ -47,3 +47,10 @@ input.addEventListener('change', () => {
   
     reader.readAsText(file);
 });
+document.addEventListener("keydown", function (event) {
+	if (event.ctrlKey && event.key == "e"){
+	document.getElementById("text").focus();}
+	if (event.ctrlKey && event.key == "y") {
+		update();
+	}
+});
