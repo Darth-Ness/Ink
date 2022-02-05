cc79c1a62ac68e008afbee534e8db79e56e4c743
Darth-Ness <jaycshflw@abine.org>
2022-02-05 07:36:35 -0500

    Added syntax highlighting!

--- main.js
+++ main.js
@@ -38,14 +38,20 @@ function swapStyleSheet(sheet){
 	document.getElementById('pagestyle').setAttribute('href', sheet);
 }
 let output = document.getElementById('output');
-let textarea = document.querySelector('textarea');
+let textarea = document.getElementById('text');
 function evalate() {
-    var lines = textarea.value.split("\n");
+    var lines = textarea.innerHTML.split("\n");
     var result = [];
     var i = 0;
+    console.log(lines);
     while (i < lines.length ) {
-        if (lines[i].indexOf("+") != -1 || lines[i].indexOf("-") != -1 || lines[i].indexOf("*") != -1 || lines[i].indexOf("/") != -1 && lines[lines[i].indexOf("/")-1] == "<") {
-            result.push(eval(lines[i]));
+        if (lines[i].indexOf("+") != -1 || lines[i].indexOf("-") != -1 || lines[i].indexOf("*") != -1 || lines[i].indexOf("/") != -1){
+            try {
+                result.push(eval(lines[i]));
+            }
+            catch(err) {
+                result.push(lines[i]);
+            }
         }
         else {result.push(lines[i]);}
         i++;
@@ -58,13 +64,16 @@ function evalate() {
         return result.join("\n").replaceAll("\n", "");
     }  
 }
-var data = evalate();
-output.innerHTML = data;
+//var data = evalate();
+//output.innerHTML = data;
 textarea.addEventListener('input', function(){
     var data = evalate();
+    var restore = saveCaretPosition(this)
+    Prism.highlightElement(this);
     console.log(data);
     TabContent[currentTab] = data;
     output.srcdoc = data;
+    restore();
 });
 var theme = document.getElementById("theme");
 window.onload = function(){  
@@ -102,4 +111,35 @@ function upload() {
 	upload.click();
 	loadFile();
 	document.body.removeChild(upload);
+}
+function saveCaretPosition(context){
+    var selection = window.getSelection();
+    var range = selection.getRangeAt(0);
+    range.setStart(  context, 0 );
+    var len = range.toString().length;
+
+    return function restore(){
+        var pos = getTextNodeAtPosition(context, len);
+        selection.removeAllRanges();
+        var range = new Range();
+        range.setStart(pos.node ,pos.position);
+        selection.addRange(range);
+
+    }
+}
+
+function getTextNodeAtPosition(root, index){
+    const NODE_TYPE = NodeFilter.SHOW_TEXT;
+    var treeWalker = document.createTreeWalker(root, NODE_TYPE, function next(elem) {
+        if(index > elem.textContent.length){
+            index -= elem.textContent.length;
+            return NodeFilter.FILTER_REJECT
+        }
+        return NodeFilter.FILTER_ACCEPT;
+    });
+    var c = treeWalker.nextNode();
+    return {
+        node: c? c: root,
+        position: index
+    };
 }
