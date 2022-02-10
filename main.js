36f0384b3279fd98381832e4b115a2c2b8e8e4fd
Darth-Ness <jaycshflw@abine.org>
2022-02-10 18:00:17 -0500

    Updatde to 3.3!
    Which finally gives good editing experience with syntax highlighting.

--- main.js
+++ main.js
@@ -5,9 +5,12 @@ var currentTab = 0;
 var lasttab = 0
 var TabContent = ["hello there"];
 function changeTab(tabA) {
-    textarea.innerText =  TabContent[tabA-1];
+    var pre = document.querySelector("pre");
+    textarea.value = TabContent[tabA-1];
+    pre.innerText =  TabContent[tabA-1];
     document.getElementById(lasttab).setAttribute('class', 'none');  
     currentTab = tabA-1;
+    console.log(currentTab);
     lasttab = currentTab;
     document.getElementById(currentTab).setAttribute('class', 'button-highlight');  
     output.srcdoc = evalate(); 
@@ -27,32 +30,23 @@ function save() {
     var download = document.createElement('a');
     download.setAttribute('href', 'data:text/html;charset=utf-8,' + encodeURIComponent(document.getElementById("text").value.replaceAll("\n", "<br>")));
     download.setAttribute('download', fileName + ".html");
-
     download.style.display = 'none';
     document.body.appendChild(download);
-
     download.click();
-
     document.body.removeChild(download);
 }
-function swapStyleSheet(sheet){
-	document.getElementById('pagestyle').setAttribute('href', sheet);
-}
+function swapStyleSheet(sheet){document.getElementById('pagestyle').setAttribute('href', sheet);}
 let output = document.getElementById('output');
 let textarea = document.getElementById('text');
 function evalate() {
-    var lines = textarea.innerText.split("\n");
+    var lines = textarea.value.split("\n");
     var result = [];
     var i = 0;
     console.log(lines);
     while (i < lines.length ) {
         if (lines[i].indexOf("+") != -1 || lines[i].indexOf("-") != -1 || lines[i].indexOf("*") != -1 || lines[i].indexOf("/") != -1){
-            try {
-                result.push(eval(lines[i]));
-            }
-            catch(err) {
-                result.push(lines[i]);
-            }
+            try {result.push(eval(lines[i]));}
+            catch(err) {result.push(lines[i]);}
         }
         else {result.push(lines[i]);}
         i++;
@@ -61,28 +55,23 @@ function evalate() {
     if (resultS.indexOf("<html>") == -1 && resultS.indexOf("<style>")  == -1 && resultS.indexOf("<script>") == -1) {
         return result.join("\n").replaceAll("\n", "<br>");
     }
-    else {
-        return result.join("\n").replaceAll("\n", "");
-    }  
+    else {return result.join("\n").replaceAll("\n", "");}  
 }
 var data = evalate();
 output.srcdoc = data;
 textarea.addEventListener('input', function(){
     var data = evalate();
-    var restore = saveCaretPosition(this)
-    Prism.highlightElement(this);
-    console.log(data);
-    TabContent[currentTab] = textarea.innerText;
+    var pre = document.querySelector("pre");
+    pre.innerHTML = textarea.value;
+    Prism.highlightElement(textarea);
+    console.log(currentTab);
+    console.log(TabContent);
+    TabContent[currentTab] = textarea.value;
     output.srcdoc = data;
-    restore();
 });
 var theme = document.getElementById("theme");
-window.onload = function(){  
-theme.selectedIndex = "3";  
-}  
-theme.addEventListener('change', function(){
-    swapStyleSheet("https://aquacss.darth-ness.repl.co/themes/" + theme.value + ".css");
-});
+window.onload = function(){theme.selectedIndex = "3";}  
+theme.addEventListener('change', function(){swapStyleSheet("https://aquacss.darth-ness.repl.co/themes/" + theme.value + ".css");});
 function loadFile() {
 let input = document.querySelector('input');
 input.addEventListener('change', () => {
@@ -112,35 +101,4 @@ function upload() {
 	upload.click();
 	loadFile();
 	document.body.removeChild(upload);
-}
-function saveCaretPosition(context){
-    var selection = window.getSelection();
-    var range = selection.getRangeAt(0);
-    range.setStart(  context, 0 );
-    var len = range.toString().length;
-
-    return function restore(){
-        var pos = getTextNodeAtPosition(context, len);
-        selection.removeAllRanges();
-        var range = new Range();
-        range.setStart(pos.node ,pos.position);
-        selection.addRange(range);
-
-    }
-}
-
-function getTextNodeAtPosition(root, index){
-    const NODE_TYPE = NodeFilter.SHOW_TEXT;
-    var treeWalker = document.createTreeWalker(root, NODE_TYPE, function next(elem) {
-        if(index > elem.textContent.length){
-            index -= elem.textContent.length;
-            return NodeFilter.FILTER_REJECT
-        }
-        return NodeFilter.FILTER_ACCEPT;
-    });
-    var c = treeWalker.nextNode();
-    return {
-        node: c? c: root,
-        position: index
-    };
 }
